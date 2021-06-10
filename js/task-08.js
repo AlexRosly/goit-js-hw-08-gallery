5667b10a5b0c89a4e9dacc48357eb3552be0ff79
AlexRosly <rosly.a777@gmail.com>
2021-06-10 05:29:09 +0300

    Added solve of task

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,22 @@
 import images from './gallery-items.js'
-
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+// Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того,
+// чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+// Закрытие модального окна по клику на div.lightbox__overlay.
+// Закрытие модального окна по нажатию клавиши ESC.
 
 const galleryContainer = document.querySelector('.js-gallery');
 const imageCard = createImageCards(images);
 const modalWindow = document.querySelector('.js-lightbox');
 const closeModalWindowBtn = document.querySelector('[data-action="close-lightbox"]');
-    const lightboxImage = document.querySelector('.lightbox__image');
+const lightboxImage = document.querySelector('.lightbox__image');
+const divOverlay = document.querySelector('.lightbox__overlay');
 
+//Create image Element
 
 galleryContainer.insertAdjacentHTML('beforeend', imageCard)
 
@@ -30,48 +40,53 @@ function createImageCards(images) {
     .join('');
 }
 
+galleryContainer.addEventListener('click', onCardClick);
 
-galleryContainer.addEventListener('click', openModalWindowByClick);
+function onCardClick(e) {
 
-function openModalWindowByClick(e) {
     e.preventDefault();
 
-    if (e.target.nodeName === 'IMG') {
-        modalWindow.classList.add('is-open');
+    const galleryImgEl = e.target;
+    if (galleryImgEl.nodeName !== 'IMG') {
+        return;
     }
+    openModalWindowByClick(e.target.dataset.source);
+}
 
-    lightboxImage.src = e.target.getAttribute('data-source');
-    lightboxImage.alt = e.target.alt;
-//     console.log(e.target);
-//     if (!e.target.classList.contains('.gallery__image')) {
-//         return;
-//     }
+//Open modal window
 
-//     modalWindow.classList.add('.is-open');
-//     // const openWindow = e.target;
-
-// console.log(e.target);
-
+function openModalWindowByClick(src) {
+    window.addEventListener('keydown', onEscKeyPress)
+    
+    modalWindow.classList.add('is-open');
+    lightboxImage.src = src;
 };
 
-closeModalWindowBtn.addEventListener('click', closeModalWindowByClick);
+// Close modal window
+    //by click on the bnt
 
-function closeModalWindowByClick() {
-    // if (e.target.nodeName === 'BUTTON') {
-    //     modalWindow.classList.remove('is-open');
+closeModalWindowBtn.addEventListener('click', closeModalWindow);
 
-    // }
+function closeModalWindow() {
+    window.removeEventListener('keydown', onEscKeyPress)
     modalWindow.classList.remove('is-open');
     lightboxImage.src = '';
 }
 
-keyClosed.addEventListener('keydown', onEscKeyPress);
+    //by click on the overlay
 
-function onEscKeyPress(e) {
-    if (e.code === 'ESCAPE') {
-        closeModalWindowByClick();
+divOverlay.addEventListener('click', onOverlayClick);
+
+function onOverlayClick(e) {
+    if (e.currentTarget === e.target) {
+        closeModalWindow();
     }
 }
 
+    //by EscKey
 
-
+function onEscKeyPress(e) {
+    if (e.code === 'Escape') {
+        closeModalWindow();
+    }
+}
